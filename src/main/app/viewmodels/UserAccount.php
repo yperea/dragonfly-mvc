07f888dc2c02c20a24acff2f1f6dfdd97fa25b09
Yesid Perea <yesid.perea@outlook.com>
2019-09-21 12:46:28 -0500

    Adding UserAccount.php ViewModel for Sign Up process.

---
+++ src/main/app/viewmodels/UserAccount.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace Dragonfly\App\ViewModels;
+
+
+class UserAccount
+{
+    private $Email;
+    private $Username;
+    private $Password;
+    private $Password2;
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->Email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->Email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->Username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->Username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->Password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->Password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword2()
+    {
+        return $this->Password2;
+    }
+
+    /**
+     * @param mixed $password2
+     */
+    public function setPassword2($password2)
+    {
+        $this->Password2 = $password2;
+    }
+
+}
