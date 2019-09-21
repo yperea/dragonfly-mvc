4cdc83d58a9cce505d50deb4afff74ce13a135be
Yesid Perea <yesid.perea@outlook.com>
2019-09-21 13:01:04 -0500

    Fixing testUser_Username method in UserTest.php.

--- src/test/app/models/UserTest.php
+++ src/test/app/models/UserTest.php
@@ -29,15 +29,14 @@ class UserTest extends TestCase
 
 
     /**
-     * @dataProvider usersData
+     * @dataProvider usernameData
      */
     public function testUser_Username($inputUser, $expected)
     {
         $user = new User();
-        $user->setUsername($inputUser->Username);
+        $user->setUsername($inputUser['Username']);
 
         $this->assertEquals($expected, $user->getUsername());
-        //$this->assertEquals($expected, $user);
     }
 
     public function usersData()
@@ -58,6 +57,27 @@ class UserTest extends TestCase
         $data = [];
         $data[] = [$this->getJsonUsersArray()[0], $user1];
         $data[] = [$this->getJsonUsersArray()[1], $user2];
+        return $data;
+    }
+
+    public function usernameData()
+    {
+
+        $user1 = new User();
+        $user1->setId(1);
+        $user1->setEmail('yperea@msn.com');
+        $user1->setUsername('yperea');
+        $user1->setPassword('$2y$10$aRHs4qxGmHaq6JVkY3eqbuxKjOPj/XLuvmGa9Bi8wm33.UMX4hbfy');
+
+        $user2 = new User();
+        $user2->setId(2);
+        $user2->setEmail('clacar@msn.com');
+        $user2->setUsername('clacar');
+        $user2->setPassword('$2y$10$aRHs4qxGmHaq6JVkY3eqbuxKjOPj/XLuvmGa9Bi8wm33.UMX4hbfy');
+
+        $data = [];
+        $data[] = [$this->getJsonUsersArray()[0], 'yperea'];
+        $data[] = [$this->getJsonUsersArray()[1], 'clacar'];
         return $data;
     }
 
