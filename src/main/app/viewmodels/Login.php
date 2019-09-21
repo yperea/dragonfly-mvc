4af2c9ecff82e11b60129d77f655e0bed2ad339f
Yesid Perea <yesid.perea@outlook.com>
2019-09-21 12:46:04 -0500

    Adding Login.php ViewModel for Sign In process.

---
+++ src/main/app/viewmodels/Login.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Dragonfly\App\ViewModels;
+
+
+class Login
+{
+    private $Username;
+    private $Password;
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->Username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->Username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->Password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->Password = $password;
+    }
+
+}
