debc5e3fbf333dfeb2ba4eb205c80afb7542077d
Yesid Perea <yesid.perea@outlook.com>
2019-09-21 12:39:00 -0500

    Adding database.php config file, to setup database connection variables.

---
+++ src/main/config/database.php
@@ -0,0 +1,10 @@
+<?php
+// Define database connection
+define('DB_DRIVER', 'mysql');
+define('DB_NAME', 'dragonflydb');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');//local -> change before deployomg
+define('DB_CHARSET', 'utf8');
+?>
+
