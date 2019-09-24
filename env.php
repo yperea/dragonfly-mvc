d098e13c2202dc7d0f38d66fcc196285ad8ffb41
Yesid Perea <yesid.perea@outlook.com>
2019-09-24 14:25:46 -0500

    Adding files and scripts for deploying on AWS.

--- env.php
+++ env.php
@@ -2,8 +2,8 @@
 $variables = [
     'APP_KEY' => '937a4a8c13e317dfd28effdd479cad2f',
     'DB_HOST' => 'localhost',
-    'DB_USERNAME' => 'root',
-    'DB_PASSWORD' => '',
+    'DB_USERNAME' => 'pdfdbuser',
+    'DB_PASSWORD' => 'pdfdbuser',
     'DB_NAME' => 'dragonflydb',
     'DB_PORT' => '3306',
     'APP_HOST' => 'dragonfly-mvc/',
--- src/main/config/database.php
+++ src/main/config/database.php
@@ -3,8 +3,8 @@
 define('DB_DRIVER', 'mysql');
 define('DB_NAME', 'dragonflydb');
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');//local -> change before deployomg
+define('DB_USER', 'dfdbuser');
+define('DB_PASS', 'dfdbuser');//local -> change before deploy
 define('DB_CHARSET', 'utf8');
 
 
